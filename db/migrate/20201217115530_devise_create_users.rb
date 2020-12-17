cdabee2dfe642a58abe0f5e52db84b57cdd8236e
jhake <73086064+jhake-papa@users.noreply.github.com>
2020-12-17 20:34:12 +0800

    fix syntax error

--- db/migrate/20201217115530_devise_create_users.rb
+++ db/migrate/20201217115530_devise_create_users.rb
@@ -3,8 +3,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :name,
-      t.integer :role_id,
+      t.string :name
+      t.integer :role_id
 
       ## Database authenticatable
       t.string :email,              null: false, default: ""
