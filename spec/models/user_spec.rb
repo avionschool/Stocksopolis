4717ecf6ad7bccf0d9434410260b5a46bcfbcd4a
jhake <73086064+jhake-papa@users.noreply.github.com>
2020-12-17 20:39:14 +0800

    add user model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validation" do
+    it "1. is not valid without name" do
+      user = User.new
+      user.save
+      expect(user).to_not be_valid 	
+      expect(user.errors).to be_present
+      expect(user.errors.to_h.keys).to include(:name)
+		end
+    it "2. is not valid without role id" do
+      user = User.new
+      user.save
+      expect(user).to_not be_valid 	
+      expect(user.errors).to be_present
+      expect(user.errors.to_h.keys).to include(:role_id)
+		end
+  end
 end
