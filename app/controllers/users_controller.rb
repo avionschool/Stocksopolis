e222d14f90d6b6f55de6ca0b0dbe7a2a39dd67e5
lfigueras <lovelyfigueras@gmail.com>
2020-12-19 14:24:04 +0800

    removed user_id from roles db schema.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     def index
         @roles = Role.all
+      
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_031507) do
+ActiveRecord::Schema.define(version: 2020_12_19_062249) do
 
   create_table "roles", force: :cascade do |t|
     t.string "role_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
