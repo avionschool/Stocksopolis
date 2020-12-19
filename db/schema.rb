fa9ef8b545894bbf831dc6804c83142e204df9ae
lfigueras <lovelyfigueras@gmail.com>
2020-12-19 12:45:17 +0800

    Fixed the db of devise to save newly added attributes, can now display username on user index.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_115530) do
+ActiveRecord::Schema.define(version: 2020_12_19_031507) do
 
   create_table "roles", force: :cascade do |t|
     t.string "role_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
