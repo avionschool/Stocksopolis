5d98072a6ff470bb6c005e43f4cc16fab2ac53b3
lfigueras <lovelyfigueras@gmail.com>
2020-12-21 20:46:16 +0800

    Fixed role seeds, refactor the Role controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
     def index
         @roles = Role.all
-        @role_name = Role.find(current_user.role_id).role_name
+        @role_name = @roles.find(current_user.role_id).role_name
+       
     end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,6 @@
 class Role < ApplicationRecord
     has_many :users
     validates :role_name, presence: true
+
+    
 end
