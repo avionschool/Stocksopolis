4e2e62eb0038d35879d2ab33ed12ea60c6dff96d
lfigueras <lovelyfigueras@gmail.com>
2020-12-21 21:09:33 +0800

    Fixed controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
     def index
         @roles = Role.all
-        @role_name = @roles.find(current_user.role_id).role_name
+        @role_name = current_user.role.role_name
        
     end
 end
