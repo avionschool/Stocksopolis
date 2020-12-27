fb0956c1097130b9998310a5b6d5c2c7b8c2a539
lfigueras <lovelyfigueras@gmail.com>
2020-12-27 23:41:42 +0800

    feature/Installed IEX

---
+++ config/initializers/iex_client.rb
@@ -0,0 +1,5 @@
+@client = IEX::Api::Client.new(
+  publishable_token: 'pk_78fe637629224f02af0d9b556b31dc04',
+  secret_token: 'sk_4d962b86775541f497caa3315aae5794',
+  endpoint: 'https://cloud.iexapis.com/v1'
+)
