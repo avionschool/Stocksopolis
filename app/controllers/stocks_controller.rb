32f3e19a611c11631369d76e6734abbce903c778
lfigueras <lovelyfigueras@gmail.com>
2021-01-02 02:24:21 +0800

    Added navbar design, implemented search method.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,27 +1,14 @@
-class UsersController < ApplicationController
+class StocksController < ApplicationController
     before_action :authenticate_user!
     before_action :load_api
     
-    def index
-        @roles = Role.all
-        @role_name = current_user.role.role_name
+    def show
         uri = URI('https://cloud.iexapis.com/stable/stock/market/list/mostactive?token=pk_78fe637629224f02af0d9b556b31dc04&listLimit=100')
         @request_api = Net::HTTP.get(uri)
         @most_active = JSON.parse(@request_api)
-       
     end
 
 
-    def search
-        if params[:search].blank?  
-            redirect_to(root_path, alert: "Empty field!") and return   
-    else  
-        @parameter = params[:search].upcase  
-    
-      end  
-    end
-
-    private
     def load_api
         @client = IEX::Api::Client.new(
             publishable_token: ENV['SAND_BOX_KEY'],
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
  
   devise_for :users
   resources :roles
-  get 'user/index'
+  resources :stocks
+
   root 'users#index'
- 
+
+  get '/search' => 'users#search', :as => 'search_page'
 end
