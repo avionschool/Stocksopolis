1f104c316cb7bc3184f4b47583b09ccf1835fcb1
lfigueras <lovelyfigueras@gmail.com>
2021-01-01 23:56:07 +0800

    This is a new commit for what I originally planned to be an amendmend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
+
+    
     def index
         @roles = Role.all
         @role_name = current_user.role.role_name
+        uri = URI('https://cloud.iexapis.com/stable/stock/market/list/mostactive?token=pk_78fe637629224f02af0d9b556b31dc04&listLimit=100')
+        @request_api = Net::HTTP.get(uri)
+        @most_active = JSON.parse(@request_api)
        
     end
+
+
+    def load_api
+        @client = IEX::Api::Client.new(
+            publishable_token: ENV['SAND_BOX_KEY'],
+            endpoint: 'https://cloud.iexapis.com/v1'
+          )
+    end
+
+
 end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-role_list = [["Admin"],["Buyer"],["Broker"]]
+role_list = ["Buyer","Broker"]
 role_list.each do |role_name|
     Role.create(role_name: role_name)
 end
+
+
+admin_user = User.new({ name: "Admin User", email: 'admin@rubyonrails.com',
+    password: 'password123', password_confirmation: 'password123', role_id: 1})
+
+    if admin_user.valid?
+        admin_user.save()
+  
+      elsif admin_user.errors.any?
+        admin_user.errors.full_messages.each do |msg|
+          puts msg
+        end
+      else
+        puts "****NOT VALID****"
+      end
