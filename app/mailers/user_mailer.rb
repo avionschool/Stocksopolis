33fc73cac3b0d62d21c1434dac066059779f5014
jhake <73086064+jhake-papa@users.noreply.github.com>
2021-01-07 15:40:27 +0800

    add mail for status change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,8 @@ class UserMailer < ApplicationMailer
     @user = params[:user]
     mail(to: @user.email, subject: "Welcome")
   end
+  def status_changed_email
+    @user = params[:user]
+    mail(to: @user.email, subject: "Account #{@user.status}")
+  end
 end
