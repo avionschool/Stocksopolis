d8ce69d795212d749823a167d34db0f59469e96d
jhake <73086064+jhake-papa@users.noreply.github.com>
2021-01-07 15:40:07 +0800

    add users toggle status route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
  
   devise_for :users
   resources :roles
+
+  patch 'users/:id/toggle_status', to: 'users#toggle_status', as: 'user_toggle_status'
   get 'user/index'
   root 'users#index'
  
